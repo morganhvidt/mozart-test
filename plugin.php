9d8a03d58f26d75b5eeae4b36c3983a9d88b154f
Morgan <10958823+morganhvidt@users.noreply.github.com>
2021-12-04 12:28:02 +0800

    Create plugin.php

---
+++ plugin.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Morgan\SL;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+class Morgan_SL {
+   public function run() {
+      return 'Hello World';
+   }
+}
